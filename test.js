448d0e60a38e31763eeac2a31cae66a0b7bd6924
Tri Nguyen <tri@tridnguyen.com>
2021-07-11 13:30:02 -0400

    Use Intl formatter to format date with precision

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 import getDateInTz from "./index.js";
-
+import { endOfMonth } from "date-fns";
 import { expect } from "chai";
 
 const TIMEZONE = "America/Chicago";
+const summerNight = "2021-07-01 20:05";
+const winterNight = "2021-12-05 20:05";
 
 describe("tz", () => {
   it("get date in tz", () => {
-    const summerNight = "2021-07-01 20:05";
     expect(getDateInTz(summerNight, TIMEZONE).toISOString()).to.equal(
       "2021-07-02T01:05:00.000Z"
     );
     expect(getDateInTz(new Date(summerNight), TIMEZONE).toISOString()).to.equal(
       "2021-07-02T01:05:00.000Z"
     );
-    const winterNight = "2021-12-05 20:05";
     expect(getDateInTz(winterNight, TIMEZONE).toISOString()).to.equal(
       "2021-12-06T02:05:00.000Z"
     );
@@ -23,7 +23,7 @@ describe("tz", () => {
   });
   it("lower time fidelity", () => {
     expect(
-      getDateInTz(new Date("2021-07-01T01:59:59.999Z"), TIMEZONE).toISOString()
-    ).to.equal("2021-07-01T03:00:00.998Z");
+      getDateInTz(endOfMonth(new Date(summerNight)), TIMEZONE).toISOString()
+    ).to.equal("2021-08-01T04:59:59.999Z");
   });
 });
