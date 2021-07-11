04c440f67c62f9156339ee5fece44ea803599acb
Tri Nguyen <tri@tridnguyen.com>
2021-07-11 13:32:22 -0400

    Use Intl formatter to improve precision (#1)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 export default function getDateInTZ(date, tz) {
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    fractionalSecondDigits: 3,
+    timeZone: tz,
+  });
   const localDate = new Date(date);
-  const localDateAtTZ = new Date(
-    localDate.toLocaleString("en-US", {
-      timeZone: tz,
-    })
-  );
+  const localDateAtTZ = new Date(formatter.format(localDate));
   const tzOffset = localDate.getTime() - localDateAtTZ.getTime();
   return new Date(localDate.getTime() + tzOffset);
 }
--- test.js
+++ test.js
@@ -1,24 +1,29 @@
 import getDateInTz from "./index.js";
-
+import { endOfMonth } from "date-fns";
 import { expect } from "chai";
 
 const TIMEZONE = "America/Chicago";
+const summerNight = "2021-07-01 20:05";
+const winterNight = "2021-12-05 20:05";
 
 describe("tz", () => {
   it("get date in tz", () => {
-    const summerNight = "2021-07-01 20:05";
     expect(getDateInTz(summerNight, TIMEZONE).toISOString()).to.equal(
       "2021-07-02T01:05:00.000Z"
     );
     expect(getDateInTz(new Date(summerNight), TIMEZONE).toISOString()).to.equal(
       "2021-07-02T01:05:00.000Z"
     );
-    const winterNight = "2021-12-05 20:05";
     expect(getDateInTz(winterNight, TIMEZONE).toISOString()).to.equal(
       "2021-12-06T02:05:00.000Z"
     );
     expect(getDateInTz(new Date(winterNight), TIMEZONE).toISOString()).to.equal(
       "2021-12-06T02:05:00.000Z"
     );
+  });
+  it("lower time fidelity", () => {
+    expect(
+      getDateInTz(endOfMonth(new Date(summerNight)), TIMEZONE).toISOString()
+    ).to.equal("2021-08-01T04:59:59.999Z");
   });
 });
