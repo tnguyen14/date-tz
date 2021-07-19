6b2b698964b5f8f6e81fa75e0b7a1666acc60d96
Tri Nguyen <tri@tridnguyen.com>
2021-07-19 14:53:31 -0400

    update label

--- README.md
+++ README.md
@@ -9,5 +9,14 @@ import getDateInTz from "@tridnguyen/date-tz";
 
 getDateInTz("2021-07-01 20:05", "America/Chicago").toISOString(); // 2021-07-02T01:05:00.000Z
 getDateInTz(new Date("2021-12-05 20:05"), "America/Chicago").toISOString(); // 2021-12-06T02:05:00.000Z
-getDateInTz(new Date("2021-12-06T02:05:00.000Z")).toISOString(); // 2021-12-06T02:05:00.000Z
+
+// new Date() will convert to local time
+getDateInTz(
+  new Date("2021-12-06T02:05:00.000Z"),
+  "America/New_York"
+).toISOString(); // 2021-12-06T02:05:00.000Z if local time is NY
+getDateInTz(
+  new Date("2021-12-06T02:05:00.000Z"),
+  "America/New_York"
+).toISOString(); // 2021-12-06T07:05:00.000Z if local time is UTC
 ```
--- test.js
+++ test.js
@@ -2,39 +2,37 @@ import getDateInTz from "./index.js";
 import { endOfMonth } from "date-fns";
 import { expect } from "chai";
 
-const TIMEZONE = "America/Chicago";
+const CHICAGO = "America/Chicago";
+const NEWYORK = "America/New_York";
 const summerNight = "2021-07-01 20:05";
 const winterNight = "2021-12-05 20:05";
 
 describe("tz", () => {
   it("get date in tz", () => {
-    expect(getDateInTz(summerNight, TIMEZONE).toISOString()).to.equal(
+    expect(getDateInTz(summerNight, CHICAGO).toISOString()).to.equal(
       "2021-07-02T01:05:00.000Z"
     );
-    expect(getDateInTz(new Date(summerNight), TIMEZONE).toISOString()).to.equal(
+    expect(getDateInTz(new Date(summerNight), CHICAGO).toISOString()).to.equal(
       "2021-07-02T01:05:00.000Z"
     );
-    expect(getDateInTz(winterNight, TIMEZONE).toISOString()).to.equal(
+    expect(getDateInTz(winterNight, CHICAGO).toISOString()).to.equal(
       "2021-12-06T02:05:00.000Z"
     );
-    expect(getDateInTz(new Date(winterNight), TIMEZONE).toISOString()).to.equal(
+    expect(getDateInTz(new Date(winterNight), CHICAGO).toISOString()).to.equal(
       "2021-12-06T02:05:00.000Z"
     );
   });
   it("date-fns methods", () => {
     expect(
-      getDateInTz(endOfMonth(new Date(summerNight)), TIMEZONE).toISOString()
+      getDateInTz(endOfMonth(new Date(summerNight)), CHICAGO).toISOString()
     ).to.equal("2021-08-01T04:59:59.999Z");
   });
   it("don't change date", () => {
     expect(
-      getDateInTz("2021-12-06T02:05:00.000Z", "America/New_York").toISOString()
+      getDateInTz("2021-12-06T02:05:00.000Z", NEWYORK).toISOString()
     ).to.equal("2021-12-06T02:05:00.000Z");
     expect(
-      getDateInTz(
-        new Date("2021-12-06T02:05:00.000Z"),
-        "America/New_York"
-      ).toISOString()
+      getDateInTz(new Date("2021-12-06T02:05:00.000Z"), NEWYORK).toISOString()
     ).to.equal("2021-12-06T02:05:00.000Z");
   });
 });
